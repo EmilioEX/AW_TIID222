6895d5fa514c9cf2ce1943992694a73d4dfb10f9
Emilio R <125053857@upq.edu.mx>
2026-09-17 17:18:49 -0600

    Introducción a PHP
    Trabajamos dentro de un archivo con extensión .php en el que incluimos html y css a través de etiquetas

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- <?php
+    echo "Holiwis";
+?> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color: blue;
+        }
+        div{
+            background: #970fcc;
+        }
+
+
+    </style>
+</head>
+<body>
+    <h1>Título</h1>
+    <h2>Título</h2>
+    <h3>Título</h3>
+    <h4>Título</h4>
+    <h5>Título</h5>
+    <h6>Título</h6>
+
+    <div>
+        <h2>Título del párrafo</h2>
+        <p>Este es un párrafo</p>
+    </div>
+</body>
+</html>
